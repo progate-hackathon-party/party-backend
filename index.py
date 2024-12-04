80b81b88a2c102e82670e6fb0d2765bc88d7a376
mono0218 <mono@monokamo.com>
2024-12-04 22:52:18 +0900

    Add: UsersRoute

--- index.py
+++ index.py
@@ -8,5 +8,21 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/users")
+async def root():
+    return {"users": ["John", "Doe"]}
+
+@app.post("/users")
+async def root():
+    return {"message": "User created successfully"}
+
+@app.put("/users")
+async def root():
+    return {"message": "User updated"}
+
+@app.delete("/users")
+async def root():
+    return {"message": "User deleted"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
